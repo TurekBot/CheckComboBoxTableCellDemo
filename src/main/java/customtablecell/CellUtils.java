46b8bb96adc6e9e2a4b9ac48112ee08c6aeca328
Bradley Turek <TurekBot@gmail.com>
2017-11-17 13:29:11 -0700

    Commented out things having to do with TextFields and ChoiceBoxes, as they don't apply.

--- src/main/java/customtablecell/CellUtils.java
+++ src/main/java/customtablecell/CellUtils.java
@@ -120,57 +120,57 @@ class CellUtils {
      * ChoiceBox convenience                                                   *
      *                                                                         *
      **************************************************************************/
-
-    static <T> void updateItem(final Cell<T> cell,
-                               final StringConverter<T> converter,
-                               final ChoiceBox<T> choiceBox) {
-        updateItem(cell, converter, null, null, choiceBox);
-    }
-
-    static <T> void updateItem(final Cell<T> cell,
-                               final StringConverter<T> converter,
-                               final HBox hbox,
-                               final Node graphic,
-                               final ChoiceBox<T> choiceBox) {
-        if (cell.isEmpty()) {
-            cell.setText(null);
-            cell.setGraphic(null);
-        } else {
-            if (cell.isEditing()) {
-                if (choiceBox != null) {
-                    choiceBox.getSelectionModel().select(cell.getItem());
-                }
-                cell.setText(null);
-
-                if (graphic != null) {
-                    hbox.getChildren().setAll(graphic, choiceBox);
-                    cell.setGraphic(hbox);
-                } else {
-                    cell.setGraphic(choiceBox);
-                }
-            } else {
-                cell.setText(getItemText(cell, converter));
-                cell.setGraphic(graphic);
-            }
-        }
-    }
-
-    ;
-
-    static <T> ChoiceBox<T> createChoiceBox(
-            final Cell<T> cell,
-            final ObservableList<T> items,
-            final ObjectProperty<StringConverter<T>> converter) {
-        ChoiceBox<T> choiceBox = new ChoiceBox<T>(items);
-        choiceBox.setMaxWidth(Double.MAX_VALUE);
-        choiceBox.converterProperty().bind(converter);
-        choiceBox.getSelectionModel().selectedItemProperty().addListener((ov, oldValue, newValue) -> {
-            if (cell.isEditing()) {
-                cell.commitEdit(newValue);
-            }
-        });
-        return choiceBox;
-    }
+//
+//    static <T> void updateItem(final Cell<T> cell,
+//                               final StringConverter<T> converter,
+//                               final ChoiceBox<T> choiceBox) {
+//        updateItem(cell, converter, null, null, choiceBox);
+//    }
+//
+//    static <T> void updateItem(final Cell<T> cell,
+//                               final StringConverter<T> converter,
+//                               final HBox hbox,
+//                               final Node graphic,
+//                               final ChoiceBox<T> choiceBox) {
+//        if (cell.isEmpty()) {
+//            cell.setText(null);
+//            cell.setGraphic(null);
+//        } else {
+//            if (cell.isEditing()) {
+//                if (choiceBox != null) {
+//                    choiceBox.getSelectionModel().select(cell.getItem());
+//                }
+//                cell.setText(null);
+//
+//                if (graphic != null) {
+//                    hbox.getChildren().setAll(graphic, choiceBox);
+//                    cell.setGraphic(hbox);
+//                } else {
+//                    cell.setGraphic(choiceBox);
+//                }
+//            } else {
+//                cell.setText(getItemText(cell, converter));
+//                cell.setGraphic(graphic);
+//            }
+//        }
+//    }
+//
+//    ;
+//
+//    static <T> ChoiceBox<T> createChoiceBox(
+//            final Cell<T> cell,
+//            final ObservableList<T> items,
+//            final ObjectProperty<StringConverter<T>> converter) {
+//        ChoiceBox<T> choiceBox = new ChoiceBox<T>(items);
+//        choiceBox.setMaxWidth(Double.MAX_VALUE);
+//        choiceBox.converterProperty().bind(converter);
+//        choiceBox.getSelectionModel().selectedItemProperty().addListener((ov, oldValue, newValue) -> {
+//            if (cell.isEditing()) {
+//                cell.commitEdit(newValue);
+//            }
+//        });
+//        return choiceBox;
+//    }
 
 
     /***************************************************************************
@@ -178,93 +178,93 @@ class CellUtils {
      * TextField convenience                                                   *
      *                                                                         *
      **************************************************************************/
-
-    static <T> void updateItem(final Cell<T> cell,
-                               final StringConverter<T> converter,
-                               final TextField textField) {
-        updateItem(cell, converter, null, null, textField);
-    }
-
-    static <T> void updateItem(final Cell<T> cell,
-                               final StringConverter<T> converter,
-                               final HBox hbox,
-                               final Node graphic,
-                               final TextField textField) {
-        if (cell.isEmpty()) {
-            cell.setText(null);
-            cell.setGraphic(null);
-        } else {
-            if (cell.isEditing()) {
-                if (textField != null) {
-                    textField.setText(getItemText(cell, converter));
-                }
-                cell.setText(null);
-
-                if (graphic != null) {
-                    hbox.getChildren().setAll(graphic, textField);
-                    cell.setGraphic(hbox);
-                } else {
-                    cell.setGraphic(textField);
-                }
-            } else {
-                cell.setText(getItemText(cell, converter));
-                cell.setGraphic(graphic);
-            }
-        }
-    }
-
-    static <T> void startEdit(final Cell<T> cell,
-                              final StringConverter<T> converter,
-                              final HBox hbox,
-                              final Node graphic,
-                              final TextField textField) {
-        if (textField != null) {
-            textField.setText(getItemText(cell, converter));
-        }
-        cell.setText(null);
-
-        if (graphic != null) {
-            hbox.getChildren().setAll(graphic, textField);
-            cell.setGraphic(hbox);
-        } else {
-            cell.setGraphic(textField);
-        }
-
-        textField.selectAll();
-
-        // requesting focus so that key input can immediately go into the
-        // TextField (see RT-28132)
-        textField.requestFocus();
-    }
-
-    static <T> void cancelEdit(Cell<T> cell, final StringConverter<T> converter, Node graphic) {
-        cell.setText(getItemText(cell, converter));
-        cell.setGraphic(graphic);
-    }
-
-    static <T> TextField createTextField(final Cell<T> cell, final StringConverter<T> converter) {
-        final TextField textField = new TextField(getItemText(cell, converter));
-
-        // Use onAction here rather than onKeyReleased (with check for Enter),
-        // as otherwise we encounter RT-34685
-        textField.setOnAction(event -> {
-            if (converter == null) {
-                throw new IllegalStateException(
-                        "Attempting to convert text input into Object, but provided "
-                                + "StringConverter is null. Be sure to set a StringConverter "
-                                + "in your cell factory.");
-            }
-            cell.commitEdit(converter.fromString(textField.getText()));
-            event.consume();
-        });
-        textField.setOnKeyReleased(t -> {
-            if (t.getCode() == KeyCode.ESCAPE) {
-                cell.cancelEdit();
-                t.consume();
-            }
-        });
-        return textField;
-    }
+//
+//    static <T> void updateItem(final Cell<T> cell,
+//                               final StringConverter<T> converter,
+//                               final TextField textField) {
+//        updateItem(cell, converter, null, null, textField);
+//    }
+//
+//    static <T> void updateItem(final Cell<T> cell,
+//                               final StringConverter<T> converter,
+//                               final HBox hbox,
+//                               final Node graphic,
+//                               final TextField textField) {
+//        if (cell.isEmpty()) {
+//            cell.setText(null);
+//            cell.setGraphic(null);
+//        } else {
+//            if (cell.isEditing()) {
+//                if (textField != null) {
+//                    textField.setText(getItemText(cell, converter));
+//                }
+//                cell.setText(null);
+//
+//                if (graphic != null) {
+//                    hbox.getChildren().setAll(graphic, textField);
+//                    cell.setGraphic(hbox);
+//                } else {
+//                    cell.setGraphic(textField);
+//                }
+//            } else {
+//                cell.setText(getItemText(cell, converter));
+//                cell.setGraphic(graphic);
+//            }
+//        }
+//    }
+//
+//    static <T> void startEdit(final Cell<T> cell,
+//                              final StringConverter<T> converter,
+//                              final HBox hbox,
+//                              final Node graphic,
+//                              final TextField textField) {
+//        if (textField != null) {
+//            textField.setText(getItemText(cell, converter));
+//        }
+//        cell.setText(null);
+//
+//        if (graphic != null) {
+//            hbox.getChildren().setAll(graphic, textField);
+//            cell.setGraphic(hbox);
+//        } else {
+//            cell.setGraphic(textField);
+//        }
+//
+//        textField.selectAll();
+//
+//        // requesting focus so that key input can immediately go into the
+//        // TextField (see RT-28132)
+//        textField.requestFocus();
+//    }
+//
+//    static <T> void cancelEdit(Cell<T> cell, final StringConverter<T> converter, Node graphic) {
+//        cell.setText(getItemText(cell, converter));
+//        cell.setGraphic(graphic);
+//    }
+//
+//    static <T> TextField createTextField(final Cell<T> cell, final StringConverter<T> converter) {
+//        final TextField textField = new TextField(getItemText(cell, converter));
+//
+//        // Use onAction here rather than onKeyReleased (with check for Enter),
+//        // as otherwise we encounter RT-34685
+//        textField.setOnAction(event -> {
+//            if (converter == null) {
+//                throw new IllegalStateException(
+//                        "Attempting to convert text input into Object, but provided "
+//                                + "StringConverter is null. Be sure to set a StringConverter "
+//                                + "in your cell factory.");
+//            }
+//            cell.commitEdit(converter.fromString(textField.getText()));
+//            event.consume();
+//        });
+//        textField.setOnKeyReleased(t -> {
+//            if (t.getCode() == KeyCode.ESCAPE) {
+//                cell.cancelEdit();
+//                t.consume();
+//            }
+//        });
+//        return textField;
+//    }
 
 
     /***************************************************************************
